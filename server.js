7e1ae98bd8a87d3a9f145728a4e67febfa3102e8
Alex Webb <alexwebbb@protonmail.com>
2017-12-12 05:09:35 -0600

    prep for refactor

---
+++ server.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+const shoppingListRouter = require('./shoppingListRouter');
+
+// log the http layer
+app.use(morgan('common'));
+
+
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+app.use('/shopping-list', shoppingListRouter);
+
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
